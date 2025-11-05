ed58601e97de327cbdb744077916ed90ae124eb1
Abdulbaki <Salausmart132@gmail.com>
2025-11-05 09:56:19 +0200

    initial commit

---
+++ src/main/kotlin/com/salausmart/chatbrew_backend/ChatbrewBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.salausmart.chatbrew_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatbrewBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChatbrewBackendApplication>(*args)
+}
