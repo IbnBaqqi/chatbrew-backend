68b789edb45c5e2c832eefe714b77814642dcad7
Abdulbaki <Salausmart132@gmail.com>
2025-11-10 17:29:34 +0200

    chat memory kinda working

---
+++ src/main/kotlin/com/salausmart/chatbrew_backend/AppConfig.kt
@@ -0,0 +1,22 @@
+package com.salausmart.chatbrew_backend
+
+import org.springframework.ai.chat.memory.ChatMemory
+import org.springframework.ai.chat.memory.ChatMemoryRepository
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository
+import org.springframework.ai.chat.memory.MessageWindowChatMemory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AppConfig {
+
+    //return InMemoryChatRepository implementation of ChatMemory whenever ChatMemory is needed
+    @Bean
+    fun chatMemoryRepository(): ChatMemoryRepository = InMemoryChatMemoryRepository()
+
+    @Bean
+    fun chatMemory(): ChatMemory =
+        MessageWindowChatMemory.builder()
+            .maxMessages(20)
+            .build()
+}
--- src/main/kotlin/com/salausmart/chatbrew_backend/ChatService.kt
+++ src/main/kotlin/com/salausmart/chatbrew_backend/ChatService.kt
@@ -1,21 +1,34 @@
 package com.salausmart.chatbrew_backend
 
 import com.salausmart.chatbrew_backend.dtos.ChatRequest
+import org.slf4j.LoggerFactory
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
+import org.springframework.ai.chat.memory.ChatMemory
+import org.springframework.ai.chat.memory.ChatMemoryRepository
 import org.springframework.ai.chat.prompt.Prompt
 import org.springframework.ai.chat.prompt.PromptTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class ChatService (
-    chatClientBuilder: ChatClient.Builder
+    chatClientBuilder: ChatClient.Builder,
+    chatMemoryRepository: ChatMemoryRepository,
+    chatMemory: ChatMemory
 ){
-    private val chatClient = chatClientBuilder.build()
+    private val chatRepository = chatMemoryRepository
+    private val chatClient = chatClientBuilder
+        .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+        .build()
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     //@Todo remove the null response
     fun sendMessage(
         request : ChatRequest
     ) : String? {
+
+//        private val memory = ChatMemory.CONVERSATION_ID
+
         val promptTemplate = PromptTemplate(
             """
                 {query}.
@@ -24,11 +37,12 @@ class ChatService (
 
         val prompt: Prompt = promptTemplate.create(mapOf("query" to request.prompt))
 
-
+        val start = System.currentTimeMillis() // Testing openai speed
         val response = chatClient.prompt(prompt)
+            .advisors()
             .call()
             .content()
-
+        logger.info("OpenAI call took ${System.currentTimeMillis() - start} ms") // Testing openai speed
         return response
     }
 }
